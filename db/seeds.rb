618f84b036d94a08e8b295924c6b48672c56a171
mattjkatz <matthewkatz7@gmail.com>
2022-06-30 18:31:37 -0500

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Reptile.create({})
+Reptile.create({com_name: "Bearded Dragon", sci_name: "Pogona vitticeps", origin: "Australia", conservation_status: "Least Concern"})
+Reptile.create({com_name: "Ball Python", sci_name: "Python regius", origin: "West and Central Africa", conservation_status: "Least Concern"})
+Reptile.create({com_name: "Russian Tortoise", sci_name: "Testudo horsfieldii", origin: "Central Asia", conservation_status: "Vulnerable"})
+Reptile.create({com_name: "Tuatara", sci_name: "Sphenodon", origin: "New Zealand", conservation_status: "At Risk"})
+Reptile.create({com_name: "American Alligator", sci_name: "Alligator mississippiensis", origin: "Southeastern United States", conservation_status: "Least Concern"})
