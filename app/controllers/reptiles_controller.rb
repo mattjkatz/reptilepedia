dc1abc10c2f656f0f461032759f1c425eec72704
mattjkatz <matthewkatz7@gmail.com>
2022-07-03 12:55:00 -0500

    add create reptile method

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -10,4 +10,16 @@ class ReptilesController < ApplicationController
     render json: reptile
   end
 
+  def create
+    reptile = Reptile.new(
+      "com_name" = params[:com_name]
+      "sci_name" = params[:sci_name]
+      "class_id" = params[:class_id]
+      "origin" = params[:origin]
+      "conservation_status" = params[:conservation_status]
+    )
+    reptile.save
+    render json: reptile
+  end
+
 end
