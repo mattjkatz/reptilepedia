bfda1e84872af7d3469127d771180b92166f201b
mattjkatz <matthewkatz7@gmail.com>
2022-07-07 15:30:28 -0500

    reptile model validation

--- app/models/reptile.rb
+++ app/models/reptile.rb
@@ -1,2 +1,6 @@
 class Reptile < ApplicationRecord
+  validates :com_name, presence: true, uniqueness: true
+  validates :sci_name, presence: true, uniqueness: true
+  validates :origin, presence: true
+  validates :conservation_status, presence: true
 end
