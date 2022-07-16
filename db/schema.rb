44db5ba1767a01cf2fab4dde2bf17f7ffb45cb0e
mattjkatz <matthewkatz7@gmail.com>
2022-07-15 23:46:34 -0500

    bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_15_015705) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_020530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_015705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "classification"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "scales", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
