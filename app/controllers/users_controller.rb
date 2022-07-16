90d33db84bd42b4eb81fe736bc342e708a90a14a
mattjkatz <matthewkatz7@gmail.com>
2022-07-16 12:43:03 -0500

    fix the user controller to use the correct schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,12 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(
-      name: params["name"],
+      first_name: params["first_name"],
+      last_name: params["last_name"],
       email: params["email"],
       password: params["password"],
-      password_confirmation: params["password_confirmation"]
+      password_confirmation: params["password_confirmation"],
+      scales: 0
     )
     if user.save
       render json: { message: "User created successfully."}, status: :created
