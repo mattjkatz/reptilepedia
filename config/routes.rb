5c6ff8e9eff75368157eac605acba8fb8f6c3c05
mattjkatz <matthewkatz7@gmail.com>
2022-07-16 12:55:02 -0500

    add routes, model stuff and http.requests routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # Reptile Routes
   get "/reptiles" => "reptiles#index"
   get "/reptiles/:id" => "reptiles#show"
   post "/reptiles/" => "reptiles#create"
   patch "/reptiles/:id" => "reptiles#update"
   delete "/reptiles/:id" => "reptiles#destroy"
+  # Reptile Routes
+  get "/users" => "users#index"
+  post "/users" => "users#create"
 end
