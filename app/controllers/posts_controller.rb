db869219272c2fcda574425bf583a758999c18cd
mattjkatz <matthewkatz7@gmail.com>
2022-07-24 13:47:02 -0500

    add controller methods and http requests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,40 @@
 class PostsController < ApplicationController
+
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  def create
+    post = Post.new(
+      "caption": params[:caption],
+      "body": params[:body],
+      "tags": params[:tags],
+      "user_id": params[:user_id]
+    )
+    post.save
+    render json: post
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.caption = params[:caption] || post.caption
+    post.body = params[:body] || post.body
+    post.tags = params[:tags] || post.tags
+    post.user_id = params[:user_id] || post.user_id
+    post.save
+    render json: post
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.delete
+    render json: {message: "Post successfully removed"}
+  end
+
 end
