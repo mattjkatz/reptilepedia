c04beaef2a3a367326e8dde5d89faba940fcb964
mattjkatz <matthewkatz7@gmail.com>
2022-07-24 13:27:14 -0500

    post routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ Rails.application.routes.draw do
   post "/reptiles/" => "reptiles#create"
   patch "/reptiles/:id" => "reptiles#update"
   delete "/reptiles/:id" => "reptiles#destroy"
-  # Reptile Routes
+  # User Routes
   get "/users" => "users#index"
   post "/users" => "users#create"
+  # Post Routes
+  get "/posts" => "posts#index"
+  get "/posts/:id" => "posts#show"
+  post "/posts/" => "posts#create"
+  patch "/posts/:id" => "posts#update"
+  delete "/posts/:id" => "posts#destroy"
   # Session Login Route
   post "/sessions" => "sessions#create"
 end
